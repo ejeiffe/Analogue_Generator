1b8317646809a86f446294d235133d006508d567
Eleanor Eiffe <37684621+ejeiffe@users.noreply.github.com>
2018-07-29 18:20:27 +1000

    Added functional groups to default dictionaries.

--- initialise_dictionaries.py
+++ initialise_dictionaries.py
@@ -5,7 +5,12 @@ functional_groups = {'H': '[H]', 'Me': 'C', 'Et': 'CC', 'n-Pr': 'CCC', 'i-Pr': '
                     'NH2': 'N', 'NHMe': 'N(C)', 'N(Me)2': 'N(C)C',
                     'F': 'F', 'Cl': 'Cl', 'Br': 'Br', 'I': 'I',
                     'CF3': 'C(F)(F)(F)', 'NO2': '[N+](=O)([O-])', 'CN': '[CN]',
-                    'CHO': 'C(=O)', 'COMe': 'C(=O)(C)', 'COOH': 'C(=O)(O)', 'COOMe': 'C(=O)(OC)', 'COONH2': 'C(=O)(ON)', 'COONHMe': 'C(=O)(ONC)', 'COON(Me)2': 'C(=O)(ON(C)C)'}
+                    'CHO': 'C(=O)', 'COMe': 'C(=O)(C)', 'COOH': 'C(=O)(O)', 'COOMe': 'C(=O)(OC)', 'COONH2': 'C(=O)(ON)', 'COONHMe': 'C(=O)(ONC)', 'COON(Me)2': 'C(=O)(ON(C)C)',
+                    'Cyclohexyl': 'C1CCCCC1', 'Cyclopentyl': 'C1CCCC1', 'Cyclobutyl': 'C1CCC1', 'Cyclopropyl': 'C1CC1',
+                    'Ph': 'C1=CC=CC=C1', 'Bn': 'C(C1=CC=CC=C1)', '1-Naphthyl': 'C1=C2C(C=CC=C2)=CC=C1', '2-Naphthyl': 'C1=CC=C2C(C=CC=C2)=C1',
+                    '2-Py': 'C1=NC=CC=C1', '3-Py': 'C1=CN=CC=C1', '4-Py': 'C1=CC=NC=C1',
+                    '2-Pyrrole': 'C1=CC=CN1', '3-Pyrrole': 'C1=CNC=C1', '2-Furan': 'C1=CC=CO1', '3-Furan': 'C1=COC=C1', '2-Thiophene': 'C1=CC=CS1', '3-Thiophene': 'C1=CSC=C1',
+                    'N-Imidazole': 'N1C=NC=C1','2-(1H-Imidazole)':'C1=NC=CN1','4-(1H-Imidazole)': 'C1=CNC=N1', '5-(1H-Imidazole)': 'C1=CN=CN1', '2-(N-Me-Imidazole)':'C1=N(C)C=CN1','4-(N-Me-Imidazole)': 'C1=CN(C)C=N1', '5-(N-Me-Imidazole)': 'C1=CN(C)=CN1'}
 
 fg_sets_dict = collections.OrderedDict([('Hydrogen', ['H']),
                     ('Alkyl', ['Me', 'Et', 'n-Pr', 'i-Pr', 'n-Bu', 'sec-Bu', 'i-Bu', 't-Bu']),
@@ -13,7 +18,12 @@ fg_sets_dict = collections.OrderedDict([('Hydrogen', ['H']),
                     ('Amino', ['NH2', 'NHMe', 'N(Me)2']),
                     ('Halo', ['F', 'Cl', 'Br', 'I']),
                     ('EWG', ['CF3', 'NO2', 'CN']),
-                    ('Carbonyl', ['CHO', 'COMe', 'COOH', 'COOMe', 'COONH2', 'COONHMe', 'COON(Me)2'])])
+                    ('Carbonyl', ['CHO', 'COMe', 'COOH', 'COOMe', 'COONH2', 'COONHMe', 'COON(Me)2'])
+                    ('Cycloalkyl', ['Cyclohexyl', 'Cyclopentyl', 'Cyclobutyl', 'Cyclopropyl'])
+                    ('Aromatic', ['Ph', 'Bn', '1-Naphthyl', '2-Naphthyl'])
+                    ('Pyridyl', ['2-Py', '3-Py', '4-Py'])
+                    ('5-Membered Heteroaromatic', ['2-Pyrrole', '3-Pyrrole', '2-Furan', '3-Furan', '2-Thiophene', '3-Thiophene'])
+                    ('Imidazole', ['N-Imidazole','2-(1H-Imidazole)','4-(1H-Imidazole)', '5-(1H-Imidazole)', '2-(N-Me-Imidazole)','4-(N-Me-Imidazole)', '5-(N-Me-Imidazole)'])])
 
 
 
